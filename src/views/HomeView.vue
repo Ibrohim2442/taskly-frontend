53207def5d7e8c9694aad4b1f4c924f78bba0c6f
Ibrohim2442 <ibrohimavazov731@gmail.com>
2025-08-07 13:16:55 +0500

    install pinia and test basic counter store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
     <h1>Welcome to Taskly</h1>
+
+    <h1>Count is {{ store.count }}</h1>
+    <h2>Double is {{ store.doubleCount}}</h2>
+    <button @click="store.increment(1)">Increment</button>
+    <button @click="store.waitAndAdd">Wait + Increment</button>
   </div>
 </template>
 
 <script setup>
+import { useCounterStore } from '../stores/counter.js';
 
+const store = useCounterStore();
 </script>
